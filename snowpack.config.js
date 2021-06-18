8a502e9c887fa78bbb0a565f1cbf75efcf05ca7c
Stukalov Artem <artemss20030906@gmail.com>
2021-06-19 02:02:44 +0500

    Initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+  plugins: [
+    '@snowpack/plugin-typescript',
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv'
+  ],
+  packageOptions: {
+    polyfillNode: true,
+  },
+  mount: {
+    "public": {url: "/", static: true, resolve: false},
+    "src": {url: "/build"}
+  },
+  optimize: {
+    bundle: true,
+  },
+  devOptions: {
+    open: 'none',
+  },
+  alias: {
+    '@utils': path.join(__dirname, 'src/utils'),
+    '@components': path.join(__dirname, 'src/components'),
+  },
+  exclude: ['**/node_modules/**/*', '**/*.test.*'],
+}
